49834e96a8a14722b6f32a24644e6a9890374399
Bestin <bestinjohn1993@gmail.com>
2017-08-04 18:06:45 +0530

    Converted searchBar into container Fixed API issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-const API_KEY = 'b1b15e88fa797225412429c1c50c122a1';
+const API_KEY = 'c6edd299cd7ea165350864fdcab12b72';
 // To make the action type consistent across the App
 export const FETCH_WEATHER = 'FETCH_WEATHER';
-export const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=b1b15e88fa797225412429c1c50c122a1${API_KEY}`;
+export const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export function fetchWeather(city) {
-    const url = `${ROOT_UR}&q=${city},us`;
+    const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
     return {
         type: FETCH_WEATHER,
