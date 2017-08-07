47fbdc04c94c33fbc023ee268d977e3922cda87e
Bestin <bestinjohn1993@gmail.com>
2017-08-07 14:47:47 +0530

    Created weatherList conatiner
    added weather to App component
    Installed sparlines

---
+++ src/containers/weather_list.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        return (
+            <tr key={name}>
+                <td>{name}</td>
+            </tr>
+        );
+    }
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <td>City</td>
+                        <td>Temperature</td>
+                        <td>Pressure</td>
+                        <td>Humidity</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+/*function mapStateToProps(state) {
+    return { weather: state.weather };
+}*/
+
+// ES6 syntax
+function mapStateToProps({ weather }) {
+    return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
